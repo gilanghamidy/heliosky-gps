0309bb18be4b6f06eb0017f0b0e8e31f58f956ed
Gilang M. Hamidy <gilang.hamidy@gmail.com>
2016-06-13 16:38:17 -0700

    Add Test Project

---
+++ Heliosky.IoT.GPS/Heliosky.IoT.GPS.Test/UnitTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
+
+namespace Heliosky.IoT.GPS.Test
+{
+    [TestClass]
+    public class ParserLoadingTest
+    {
+        [TestMethod]
+        public void LoadParserComponent()
+        {
+        }
+    }
+}
