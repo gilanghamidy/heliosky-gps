cd13d04727f88412de009d552677987037642f1e
Gilang M. Hamidy <gilang.hamidy@gmail.com>
2016-06-14 16:17:32 -0700

    Develop test method for parsing GPGGA string

--- Heliosky.IoT.GPS/Heliosky.IoT.GPS.Test/UnitTest.cs
+++ Heliosky.IoT.GPS/Heliosky.IoT.GPS.Test/UnitTest.cs
@@ -1,14 +1,29 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
+using Heliosky.IoT.GPS;
 
 namespace Heliosky.IoT.GPS.Test
 {
     [TestClass]
     public class ParserLoadingTest
     {
+        
+
         [TestMethod]
         public void LoadParserComponent()
         {
+            var parser = new Model.NMEAParser();
+        }
+
+        [TestMethod]
+        public void ParseGPGGAString()
+        {
+            string str = "$GPGGA,123519,4807.038,N,01131.000,E,1,08,0.9,545.4,M,46.9,M,,*47";
+            var parser = new Model.NMEAParser();
+            var res = parser.Parse(str);
+
+            Model.FixData data = (Model.FixData)res;
+
         }
     }
 }
