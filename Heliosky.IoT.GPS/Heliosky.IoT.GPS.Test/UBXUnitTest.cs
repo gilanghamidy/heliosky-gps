ff956e6fed33a399b0d9fb7580a82e0d22068da1
Gilang M. Hamidy <gilang.hamidy@gmail.com>
2016-06-24 16:12:25 -0700

    Add UBX model and respective unit test

---
+++ Heliosky.IoT.GPS/Heliosky.IoT.GPS.Test/UBXUnitTest.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Heliosky.IoT.GPS;
+using Heliosky.IoT.GPS;
+
+namespace Heliosky.IoT.GPS.Test
+{
+    [TestClass]
+    public class UBXUnitTest
+    {
+        [TestMethod]
+        public void GenerateConfigPortMessage()
+        {
+            UBX.ConfigPort cfg_prt = new UBX.ConfigPort()
+            {
+                PortID = 1,
+                StopBit = UBX.ConfigPort.StopBitType.OneStop,
+                Parity = UBX.ConfigPort.ParityType.NoParity,
+                CharacterLength = UBX.ConfigPort.CharacterLengthType.Bit8,
+                BaudRate = 115200,
+                InputProtocol = UBX.ConfigPort.Protocol.UBX | UBX.ConfigPort.Protocol.NMEA,
+                OutputProtocol = UBX.ConfigPort.Protocol.UBX | UBX.ConfigPort.Protocol.NMEA
+            };
+
+            var generated_data = cfg_prt.ToBinaryData();
+
+            byte[] correct = { 0xB5, 0x62, 0x06, 0x00,
+                               0x14, 0x00, 0x01, 0x00,
+                               0x00, 0x00, 0xD0, 0x08,
+                               0x00, 0x00, 0x00, 0xC2,
+                               0x01, 0x00, 0x03, 0x00,
+                               0x03, 0x00, 0x00, 0x00,
+                               0x00, 0x00, 0xBC, 0x5E };
+
+            Assert.AreEqual(correct.Length, generated_data.Length);
+            
+            for(int i = 0; i < correct.Length; i++)
+            {
+                if(correct[i] != generated_data[i])
+                {
+                    Assert.Fail("Incorrect data on index: " + i);
+                    break;
+                }
+            }
+        }
+    }
+}
--- Heliosky.IoT.GPS/Heliosky.IoT.GPS/UBXModels.cs
+++ Heliosky.IoT.GPS/Heliosky.IoT.GPS/UBXModels.cs
@@ -4,9 +4,135 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Heliosky.IoT.GPS
+namespace Heliosky.IoT.GPS.UBX
 {
-    class UBXModels
+    [UBXMessage(0x06, 0x00, MessageType.Send | MessageType.Receive)]
+    public class ConfigPort : UBXModelBase
     {
+        [Flags]
+        public enum Protocol : ushort
+        {
+            UBX = 0x1,
+            NMEA = 0x2
+        }
+
+        public enum CharacterLengthType : byte
+        {
+            Bit7 = 3,
+            Bit8 = 4
+        }
+
+        public enum ParityType : byte
+        {
+            Even = 0,
+            Odd = 1,
+            NoParity = 4
+        }
+
+        public enum StopBitType : byte
+        {
+            OneStop = 0,
+            OneHalf = 1,
+            TwoStop = 2,
+            TwoHalf = 3
+        }
+
+        public ConfigPort()
+        {
+            Mode = 0x10;
+        }
+
+        [UBXField(1)]
+        public byte PortID { get; set; }
+
+        [UBXField(2)]
+        public byte Reserved1 { get; private set; }
+
+        [UBXField(3)]
+        public ushort TXReady { get; set; }
+
+        [UBXField(4)]
+        public uint Mode { get; private set; }
+
+        [UBXField(5)]
+        public uint BaudRate { get; set; }
+
+        [UBXField(6)]
+        public ushort InputProtocolMask { get; private set; }
+
+        [UBXField(7)]
+        public ushort OutputProtocolMask { get; private set; }
+
+        [UBXField(8)]
+        public ushort Reserved4 { get; private set; }
+
+        [UBXField(9)]
+        public ushort Reserved5 { get; private set; }
+
+        public Protocol InputProtocol
+        {
+            get { return (Protocol)InputProtocolMask; }
+            set { InputProtocolMask = (ushort)value; }
+        }
+
+        public Protocol OutputProtocol
+        {
+            get { return (Protocol)OutputProtocolMask; }
+            set { OutputProtocolMask = (ushort)value; }
+        }
+
+        public CharacterLengthType CharacterLength
+        {
+            get
+            {
+                byte mask = 0xC0;
+                byte typeVal = (byte)((Mode & mask) >> 6);
+                return (CharacterLengthType)typeVal;
+            }
+
+            set
+            {
+                uint mask = ~0xC0U;
+                uint clearedVal = Mode & mask;
+                uint typeVal = (uint)value << 6;
+                Mode = clearedVal | typeVal;
+            }
+        }
+
+        public ParityType Parity
+        {
+            get
+            {
+                ushort mask = 0xE00;
+                ushort typeVal = (ushort)((Mode & mask) >> 9);
+                return (ParityType)typeVal;
+            }
+
+            set
+            {
+                uint mask = ~0xE00U;
+                uint clearedVal = Mode & mask;
+                uint typeVal = (uint)value << 9;
+                Mode = clearedVal | typeVal;
+            }
+        }
+
+        public StopBitType StopBit
+        {
+            get
+            {
+                ushort mask = 0x3000;
+                ushort typeVal = (ushort)((Mode & mask) >> 12);
+                return (StopBitType)typeVal;
+            }
+
+            set
+            {
+                uint mask = ~0x3000U;
+                uint clearedVal = Mode & mask;
+                uint typeVal = (uint)value << 12;
+                Mode = clearedVal | typeVal;
+            }
+        }
     }
 }
