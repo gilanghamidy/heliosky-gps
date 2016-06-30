10ed39dccd4890bd89a9d957888a088876510a6e
Gilang M. Hamidy <gilang.hamidy@gmail.com>
2016-06-30 21:26:14 +0700

    [WIP] Raising event when message arrives

---
+++ Heliosky.IoT.GPS/Heliosky.IoT.GPS/UBX/UBXEventArgs.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Heliosky.IoT.GPS.UBX
+{
+    public class UBXMessageReceivedEventArgs : EventArgs
+    {
+        public UBXModelBase Message { get; internal set; }
+    }
+
+    public delegate void UBXExpectedMessageCallback(UBXModelBase retrievedMessage);
+}
