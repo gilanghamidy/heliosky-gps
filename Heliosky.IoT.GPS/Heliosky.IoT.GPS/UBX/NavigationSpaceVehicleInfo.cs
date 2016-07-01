04758399b12a2e1624454fa89d57051d442f0e2d
Gilang M. Hamidy <gilang.hamidy@gmail.com>
2016-06-30 17:40:47 -0700

    [WIP] Implementation for repeated block in UBX message

---
+++ Heliosky.IoT.GPS/Heliosky.IoT.GPS/UBX/NavigationSpaceVehicleInfo.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Heliosky.IoT.GPS.UBX
+{
+    [UBXMessage(0x01, 0x30, MessageType.Receive | MessageType.Poll)]
+    public class NavigationSpaceVehicleInfo
+    {
+        [UBXField(1)]
+        public uint TimeMillisOfWeek { get; set; }
+
+        [UBXField(2)]
+        public byte ChannelCount { get; set; }
+
+        [UBXField(3)]
+        public byte GlobalFlags { get; private set; }
+
+        [UBXField(4)]
+        private ushort Reserved2 { get; set; }
+
+        [UBXField(5)]
+        [UBXList(2)]
+        private IEnumerable<SpaceVehicleChannelItem> ChannelList { get; set; }
+    }
+
+    [UBXStructure]
+    public struct SpaceVehicleChannelItem
+    {
+        [UBXField(1)]
+        public byte ChannelNumber { get; set; }
+
+        [UBXField(2)]
+        public byte SatteliteID { get; set; }
+
+        [UBXField(3)]
+        public byte Flags { get; set; }
+
+        [UBXField(4)]
+        public byte Quality { get; set; }
+
+        [UBXField(5)]
+        public byte SignalStrength { get; set; }
+
+        [UBXField(6)]
+        public sbyte Elevation { get; set; }
+
+        [UBXField(7)]
+        public short Azimuth { get; set; }
+
+        [UBXField(8)]
+        public int PseudoRangeResidual { get; set; }
+    }
+}
