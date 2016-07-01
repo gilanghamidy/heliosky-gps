923cd2601d1bb403f814af029c7b9cc313582d79
Gilang M. Hamidy <gilang.hamidy@gmail.com>
2016-07-01 15:37:34 -0700

    [WIP] Map Page to display map and radar

---
+++ Heliosky.IoT.GPS/Heliosky.IoT.GPS.SampleApp/CustomControls/FillStackPanel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Foundation;
+using Windows.UI.Xaml.Controls;
+
+namespace Heliosky.IoT.GPS.SampleApp.CustomControls
+{
+    public class FillStackPanel : StackPanel
+    {
+        protected override Size MeasureOverride(Size availableSize)
+        {
+            return base.MeasureOverride(availableSize);
+        }
+        protected override Size ArrangeOverride(Size finalSize)
+        {
+            if(Orientation == Orientation.Vertical)
+            {
+                var cellWidth = finalSize.Width;
+                var itemCount = Children.Count;
+                var cellHeight = finalSize.Height / itemCount;
+
+                Size cellSize = new Size(cellWidth, cellHeight);
+
+                foreach (var item in Children)
+                {
+                    
+                }
+            }
+            else
+            {
+
+            }
+
+            return finalSize;
+        }
+    }
+}
