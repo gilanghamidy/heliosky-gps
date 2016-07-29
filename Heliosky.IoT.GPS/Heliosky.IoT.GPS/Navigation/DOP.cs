45f9165fedae3a20912d17a12dcd4be91c5af5b4
Gilang M. Hamidy <gilang.hamidy@gmail.com>
2016-07-29 14:31:49 +0700

    Add LGPL v3 License for Heliosky.IoT.GPS library

--- Heliosky.IoT.GPS/Heliosky.IoT.GPS/Navigation/DOP.cs
+++ Heliosky.IoT.GPS/Heliosky.IoT.GPS/Navigation/DOP.cs
@@ -1,8 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿/*   DOP.cs
+ *   Copyright (C) 2016 Gilang M. Hamidy (gilang.hamidy@gmail.com)
+ *   
+ *   This file is part of Heliosky.IoT.GPS
+ * 
+ *   Heliosky.IoT.GPS is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU Lesser General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   Heliosky.IoT.GPS is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU Lesser General Public License for more details.
+ *
+ *   You should have received a copy of the GNU Lesser General Public License
+ *   along with Heliosky.IoT.GPS.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 namespace Heliosky.IoT.GPS.Navigation
 {
--- Heliosky.IoT.GPS/Heliosky.IoT.GPS/Properties/AssemblyInfo.cs
+++ Heliosky.IoT.GPS/Heliosky.IoT.GPS/Properties/AssemblyInfo.cs
@@ -1,4 +1,24 @@
-﻿using System.Reflection;
+﻿/*   AssemblyInfo.cs
+ *   Copyright (C) 2016 Gilang M. Hamidy (gilang.hamidy@gmail.com)
+ *   
+ *   This file is part of Heliosky.IoT.GPS
+ * 
+ *   Heliosky.IoT.GPS is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU Lesser General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   Heliosky.IoT.GPS is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU Lesser General Public License for more details.
+ *
+ *   You should have received a copy of the GNU Lesser General Public License
+ *   along with Heliosky.IoT.GPS.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
@@ -6,11 +26,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Heliosky.IoT.GPS")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("GPS library for U-Blox Neo-6M and its variant")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Gilang M. Hamidy")]
 [assembly: AssemblyProduct("Heliosky.IoT.GPS")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyCopyright("Gilang M. Hamidy ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -24,6 +44,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.1.*")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
 [assembly: ComVisible(false)]
