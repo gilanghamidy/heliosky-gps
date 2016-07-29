a9508c11879c0d0693dc6fe05cc2e8f5c68d5650
Gilang M. Hamidy <gilang.hamidy@gmail.com>
2016-07-29 15:08:47 +0700

    Fix unit testing file and add MIT license to unit testing file.

--- Heliosky.IoT.GPS/Heliosky.IoT.GPS.Test/UnitTest.cs
+++ Heliosky.IoT.GPS/Heliosky.IoT.GPS.Test/UnitTest.cs
@@ -1,4 +1,27 @@
-﻿using System;
+﻿/* The MIT License (MIT)
+ * UnitTest.cs
+ * Copyright (c) 2016 Gilang M. Hamidy (gilang.hamidy@gmail.com)
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ */
+
+using System;
 using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
 using Heliosky.IoT.GPS.Legacy;
 
@@ -60,7 +83,7 @@ namespace Heliosky.IoT.GPS.Test
                 var res = parser.Parse(str);
                 Assert.Fail("Parsing message with invalid checksum should fail");
             }
-            catch(InvalidChecksumException)
+            catch(Legacy.InvalidChecksumException)
             {
             
             }
@@ -77,7 +100,7 @@ namespace Heliosky.IoT.GPS.Test
                 var res = parser.Parse(str);
                 Assert.Fail("Parsing message with unknown message type should fail");
             }
-            catch(UnknownMessageException)
+            catch(Legacy.UnknownMessageException)
             {
 
             }
